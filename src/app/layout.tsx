4dce161263d18ee3a6da68b32e60320d74635a56
devbayel <bayel.dev@gmail.com>
2024-07-09 14:46:06 -0700

    component improvements and sign out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body className={`flex flex-col gap-4`}>
+      <body className={`flex flex-col gap-4 p-5`}>
         <div className="w-full">
           <TopNav/>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,40 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 import { log } from "console";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic"
 
 export default async function HomePage() {
 
-  const images = await db.query.images.findMany({
-    orderBy:(model,  { desc }) => desc(model.id),
-  });
+  async function Images(){
 
-  
+    const images = await db.query.images.findMany({
+      orderBy:(model,  { desc }) => desc(model.id),
+    });
+
+    return (
+      <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48 flex flex-col">
+          <img src={image.url}/>
+          <div>{image.name}</div>
+        </div>
+      ))
+    }</div>
+    )
+  }
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
 
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
 
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col">
-            <img src={image.url}/>
-            <div>{image.name}</div>
-          </div>
-        ))
-      }</div>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
